9c000b184721c3ae348c777353450f9af9793ba9
Syed Mujtaba <mujtabac1@gmail.com>
2021-07-02 23:34:25 +0500

    Alert

---
+++ app.js
@@ -0,0 +1,12 @@
+// ALERT
+alert('Welcome to my Page')
+
+alert('Error! Please enter a valid password')
+
+alert('Welcome to js Land...\nHappy coding')
+
+alert('Welcome to Js Land....')
+alert('Happy Coding!')
+
+//=================================================================================
+
