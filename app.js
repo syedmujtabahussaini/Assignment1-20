71c6d7ddcc55ea98025b6cc7de835f2834a96243
Syed Mujtaba <mujtabac1@gmail.com>
2021-07-02 23:55:44 +0500

    Chapter2

--- app.js
+++ app.js
@@ -10,3 +10,31 @@ alert('Happy Coding!')
 
 //=================================================================================
 
+// VARIABLES FOR STRINGS
+var username
+var myname = 'Syed Mujtaba Hussaini'
+
+var message = 'Hello World'
+alert(message)
+
+var std_Name = 'Jhone Doe'
+var std_age = 15
+var std_Course = 'Certified Mobile Application Development'
+
+alert(std_Name)
+alert(std_age,'Years old')
+alert(std_Course)
+
+var itm_name = 'Pizza \nPizz \nPiz \nPi \nP'
+alert(itm_name)
+
+var email = 'mujtabac1@gmail.com'
+alert ('My email address is ' + email)
+
+var book = 'A smarter way to learn JavaScirpt'
+alert('I am trying to learn from the book' + book)
+
+document.write('Ya! I can write HTML contenet through JavaScript')
+
+var design = '“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”'
+alert(design)
